02a6d05e8ea691ae626e2f63c6e7e786c09b36bb
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-10-03 15:24:12 +0530

    Damage

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera FPcamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPcamera.transform.position, FPcamera.transform.forward, out hit, range))
+        {
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
